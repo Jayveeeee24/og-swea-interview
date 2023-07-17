cb3bb189605b453e886e8a9120a7bbae89ee5d55
Jayveeeee24 <jayveejimenez32@gmail.com>
2023-07-17 22:07:48 +0800

    Task 6: Implement the index Function in PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    
+    public function index(): JsonResponse
+    {
+        //gets all posts in [Post] model along the corresponding user and store it into posts variable
+        $posts = Post::with('user')->get(); 
+
+        // returns the actual json response that accepts the posts variable as a parameter
+        return response()->json($posts);
+    }
 }
