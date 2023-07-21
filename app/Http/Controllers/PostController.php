742e9d2530ce9b0bda5dd1b0f8a49ba390181973
Jayveeeee24 <jayveejimenez32@gmail.com>
2023-07-21 15:17:22 +0800

    Task 10: Adjust Route and Controller to Pass the Integration Test

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,8 +9,9 @@ class PostController extends Controller
 {
     public function index(): JsonResponse
     {
-        //gets all posts in [Post] model along the corresponding user and store it into posts variable
-        $posts = Post::with('user')->get(); 
+        //gets all posts in [Post] model along the corresponding user and store it into posts variable 
+        //and return it in descending order
+        $posts = Post::with('user')->orderBy('id', 'desc')->get();
 
         // returns the actual json response that accepts the posts variable as a parameter
         return response()->json($posts);
